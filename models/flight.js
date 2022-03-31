46dcb13d88f4babd08bb3ee74a2659fa9f773946
OlloClark <ollo42@hotmail.co.uk>
2022-03-31 14:53:14 -0700

    building flioght model

---
+++ models/flight.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ["American", "Delta", "Southwest", "United"]
+    },
+    airport: {
+        type: String,
+        enum: ["ATL", "DFW", "DEN", "LAX", "SAN"],
+        default: "DEN"
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: Date
+});
