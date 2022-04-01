29776e92c719c838d817ab16e0cd137cb2c182fa
OlloClark <ollo42@hotmail.co.uk>
2022-04-01 16:51:33 -0700

    adding destinationSchema

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+    airport: {
+        type: String,
+        enum: ["AUS", "DFW", "DEN", "LAX", "SAN"]
+    },
+    arrival: {
+        type: Date,
+    }
+})
+
 const flightSchema = new Schema({
     airline: {
         type: String,
@@ -26,8 +36,9 @@ const flightSchema = new Schema({
             let day = today.getDay();
         return new Date(year + 1, month, day);
         }
-
-    }}
+    },
+    destinations: [destinationSchema]
+}
 );
 
 module.exports = mongoose.model('Flight', flightSchema);
