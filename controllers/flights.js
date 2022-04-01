3bdd7bba83485baf435155e722caf79a674c6ceb
OlloClark <ollo42@hotmail.co.uk>
2022-03-31 19:01:51 -0700

    adding create function

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,7 +2,8 @@ const Flight = require("../models/flight");
 
 module.exports = {
     index,
-    new: newFlight
+    new: newFlight,
+    create
 }
 
 function index(req,res){
@@ -13,4 +14,12 @@ function newFlight(req,res){
     res.render("flights/new");
 }
 
-
+function create(re, res){
+    const flight = new Flight(req.body);
+    flight.save(function(err){
+        //suggested handling of errors
+        if (err) return res.render("flights/new");
+        //for now, redirect straight back to new.ejs
+        res.redirect("flights/new");
+    })
+}
